8c9948f896044997df25e3acc01834035a90fc9f
aa8796-tech <aa8796@fayoum.edu.eg>
2026-05-04 09:57:16 +0300

    Set up home page for Hang Man game
    Initialize game components and set up home page display.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from configurations import Config
+from ui import CLIUI, CLIUtility
+
+config = Config()
+utility = CLIUtility()
+ui = CLIUI()
+
+def home_page():
+  utility.clear_screen()
+  ui.display_header("Hang Man")
+  utility.display_message("Welcome to the Hang Man Game.")
+  utility.display_message("Follow the instructions for a pleasant user experience.", "note")
+  uitility.display_divider()
+  utility.display_menu("Instructions", instructions) # I'll write the instructions later. Don't run the program now
 
